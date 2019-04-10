c1d9fe1c1708650181c963dc9ee90215dd65b427
panda-with-issues <ryoda94@gmail.com>
2019-04-10 18:55:06 +0200

    ver 1.0

--- hash_map_implementation.py
+++ hash_map_implementation.py
@@ -4,26 +4,12 @@ Hash maps implementation in python3
 -----------------------------------
 Disclaimer: in Python we don’t have an array data structure that uses a contiguous block of memory. So I'll simulate an array by creating
 a list and keeping track of its size with an additional variable. 
-This is somewhat elaborate for the actual storage of a key-value pair, but hey, this is just an exercice to gain a deeper understanding
+This is somewhat elaborate for the actual storage of a key-value pair, but hey, this is just an exercise to gain a deeper understanding
 of the structure as it is constructed.
 For real-world use cases in which a key-value store is needed, Python offers a built-in hash table implementation with dictionaries.
 """
 
 from linked_list_implementation import Linked_list
-
-class LinkedList(Linked_list):
-
-    def __str__(self):
-        return self.strfy()
-    
-    #making list iterable in nodes's data
-    def __iter__(self):
-        data = []
-        i = self.head_node
-        while i:
-            data.append(i.data)
-            i = i.next_node
-        return data
 
 class HashMap:
 
@@ -32,88 +18,141 @@ class HashMap:
         self.array_size = array_size
         #create the array with given size
         self.array = [None for i in range(self.array_size)]
+        #prevent to hash same keys with different values in case of memory expansion
+        self.buffer = 0
+        self.size = self.array_size - self.array.count(None)
 
     #defining the hashing function
     def hash(self, key):
         
         #check if the key is hashable
-        if type(key) == int or type(key) == str:
-
-            if type(key) == str:
+        if type(key) != list and type(key) != dict and key != "deleted":
+            
+            if type(key) == tuple:
+                hash_code = 0
+                for data in key:
+                    if type(data) == str:
+                        hash_code += sum(data.encode())
+                    elif type(data) == float:
+                        hash_code += int(data)
+                    else:
+                        hash_code += data
+            elif type(key) == str:
                 hash_code = sum(key.encode())
+            elif type(key) == float:
+                hash_code = int(key)
             else:
                 hash_code = key
-            return hash_code % self.array_size
+            
+            return hash_code % (self.array_size - self.buffer)
 
         raise TypeError
 
-    #defining a squared probing for open addressng
-    def sqrd_probing(self, index):
+ #expand memory if trying to insert data into a full array
+    def expand_memory(self, index):
+        for i in range(index):
+            self.array.append(None)
+            self.array_size += 1
+            self.buffer += 1
+        
+    #defining a squared probing for open addressng and expand memory if doing too many iterations
+    def sqrd_probing(self, index, key, inserting=True):
         i = 0
-        while self.array[index] == None:
+        while self.array[index] != None:
+            if i >= 50 and inserting:
+                self.expand_memory(index)
+            if self.array[index][0] == key:
+                return index
+            if self.array[index] == "deleted" and inserting:
+                return index
             i += 1
             probing = i ** 2
             index += probing
-            if index > self.array_size:
-                index %= self.array_size
+            if index >= self.array_size:
+                if i < 50:
+                    index %= (self.array_size - self.buffer)
+                else:
+                    index %= self.array_size
         return index
 
     #allocate data in the array
     def insert(self, key, data):
         index = self.hash(key)
         bucket = self.array[index]
+        data_to_insert = [key, data]
 
-        if self.array[index] == None:
-            self.array[index] = [key, data]
-
-        #handling collisions using a linked list separate chaining for array smaller than 16
-        elif self.array_size <= 16:
-            if type(self.array[index]) != LinkedList:
-                ll = LinkedList(self.array[index])
-                self.array[index] = ll
-            ll.add_node([key, data])
-
+        #handling collisions using a linked list separate chaining for array smaller than 10
+            # 10 was chosen merely for its ease in debugging
+        if self.array_size < 10:
+            
+            if bucket == None:
+                self.array[index] = Linked_list(data_to_insert)
+                self.size += 1
+        
+            else:
+                current_node = bucket.head_node
+                while current_node:
+                    data_stored = current_node.data
+                    if data_stored[0] == key:
+                        data_stored[1] = data
+                        break
+                    if current_node.next_node == None:
+                        bucket.add_node(data_to_insert)
+                        self.size += 1
+                    current_node = current_node.next_node
+                    
         #handling collisions using a squared probing for longer arrays
+            # Obiouvsly, 10 is a too little value to open address with a squared probing.
+            # However I chose to implement square probing just to try and see if I was able to.
+            # Neither open addressing nor squared probing are efficient in this scenario, but they serve the purpose of practicing and understanding.
         else:
-            index = self.sqrd_probing(index)
-            self.array[index] = [key, data]
-
+            if self.size == len(self.array):
+                self.expand_memory(index)
+            elif self.array[index] == None or self.array[index] == "deleted":
+                self.array[index] = data_to_insert
+                self.size += 1
+            else:
+                new_index = self.sqrd_probing(index, key)
+                if self.array[new_index] == None or self.array[new_index] == "deleted":
+                    self.array[new_index] = data_to_insert
+                    self.size += 1
+                else:
+                    self.array[new_index][1] = data
+                
     #retrieve data given the key
     def get(self, key):
         index = self.hash(key)
 
-        if type(self.array[index]) == LinkedList:
-            i = self.array[index].head_node
-            data = []
-            while i:
-                print(i)
-                data.append(i.data)
-                i = i.next_node
-                print(data)
-            for pairs in data:
-                if pairs[0] == key:
-                    return pairs[1]
+        if type(self.array[index]) == Linked_list:
+            current_node = self.array[index].head_node
+            while current_node:
+                if current_node.data[0] == key:
+                    return current_node.data[1]
+                current_node = current_node.next_node
+            raise KeyError
 
         else:
-            bucket = self.array[index]
-            while bucket[0] == key:
-                index = self.sqrd_probing(index)
-            return bucket[1]
+            key_val_pair = self.array[index]
+            if key_val_pair[0] == key:
+                return key_val_pair[1]
+            else:
+                new_index = self.sqrd_probing(index, key, False)
+                key_val_pair = self.array[new_index]
+                if key_val_pair == None or key_val_pair == "deleted":
+                    raise KeyError
+                return key_val_pair[1]
 
-
-
-
-ll_map = HashMap(4)
-ll_map.insert("Sailor Moon", "Tsukino Usagi")
-ll_map.insert("Sailor Mercury", "Mizuno Ami")
-ll_map.insert("Sailor Mars", "Hino Rei")
-ll_map.insert("Sailor Jupiter", "Kino Makoto")
-ll_map.insert("Sailor Venus", "Aino Minako")
-"""
-print(ll_map.array)
-print(ll_map.array[1])
-print(ll_map.array[3])
-"""
-
-print(ll_map.get("Sailor Moon"))
-#print(ll_map.get("Sailor Jupiter"))
+    #delete the pair given the key
+    def pop(self, key):
+        data_to_remove = [key, self.get(key)] #here is where the key check is done
+        index = self.array.index(data_to_remove)
+        if type(self.array[index]) == Linked_list:
+            self.array[index].remove_node(data_to_remove)
+            self.size -= 1
+        elif self.array[index] == data_to_remove:
+            self.array[index] = "deleted"
+            self.size -= 1
+        else:
+            new_index = self.sqrd_probing(index, key, True)
+            self.array[new_index] = "deleted"
+            self.size -= 1

7895c3fd517191b2c30e585e3c56b7ca4e338c18
panda-with-issues <ryoda94@gmail.com>
2019-04-15 11:01:58 +0200

    add str rpr, logic to seet tree's root; begun logic to add node's child

--- tree_implementation.py
+++ tree_implementation.py
@@ -12,7 +12,11 @@ class TreeNode:
         #distance from root node
         self.level = None
     
-    def add_children(self, node):
+    #add child to a node 
+    def add_child(self, data):
+        new_node = __init__(data)
+        self.children
+
 
 class Tree:
     
@@ -23,3 +27,13 @@ class Tree:
             self.root.level = 0
         else:
             self.root = root
+
+    def __str__(self):
+        return "A tree with root {}".format(self.root.data)
+
+    #set tree's root node. If passed argument is not a TreeNode object, instantiate it
+    def set_root(self, root):
+        if type(root) != TreeNode:
+            root = TreeNode(root)
+        self.root = root
+        self.root.level = 0

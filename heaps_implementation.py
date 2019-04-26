2da82a9da7504db3ce4f560e9fc40ff6b38657f2
panda-with-issues <ryoda94@gmail.com>
2019-04-26 22:08:07 +0200

    add logic to update and heapify up the dataset, begin writing logic to retrieve the minimum and heapifying down

--- heaps_implementation.py
+++ heaps_implementation.py
@@ -15,6 +15,9 @@ class MinHeap:
     def __init__(self):
         self.dataset = [None]
 
+    def __str__(self):
+        return "A minimum heap"
+
     """
     helper methods
     """
@@ -30,6 +33,10 @@ class MinHeap:
     def get_right_child_index(self, index):
         child_index = index * 2 + 1
         return child_index
+
+    def has_child(self, data):
+        data_index = self.dataset.index(data)
+        return self.get_left_child_index(data_index) < len(self.dataset)
 
     """
     end helper
@@ -61,6 +68,43 @@ class MinHeap:
             self.heapify_up()
         print(self.dataset)
 
+    #sanitize the array pushing down the root untill it meets bigger children. When it's parent of two children, swap always with the smaller
+    def heapify_down(self):
+        parent_index = 1
+        while self.has_child(parent):
+            parent = self.dataset[parent_index]
+            child_index = self.get_left_child_index(parent_index)
+            child = self.dataset[child_index]
+            right_child_index = self.get_right_child_index(parent_index)
+            #if there is a right child
+            if left_child_index + 1 < len(self.dataset):
+                right_child = self.dataset[right_child_index]
+                #if right child is bigger than left one, compare parent with it instead of comparing with left child
+                if right_child < left_child:
+                    child = right_child
+                    child_index = right_child_index
+            if parent > child:
+                self.dataset[parent] = child
+                self.dataset[child_index] = parent
+                parent_index = child_index
+            else:
+                break
+
+             
+
+
+    #remove and return the minimum value into the array. The last element of the list is put in its place. Then sanitize the array heapifing down
+    def retrieve(self):
+        #check whether there is anything to return
+        if len(self.dataset) == 1:
+            print("Heap is empty")
+        else:
+            root = self.dataset[1]
+            new_root = self.dataset.pop()
+            self.dataset[1] = new_root
+            self.heapify_down()
+            return root
+
 """
 debug
 """
@@ -69,3 +113,4 @@ from random import randrange
 for i in range(10):
     mucchio.add(randrange(0,31))
 print(mucchio.dataset)
+print(self.retrieve)

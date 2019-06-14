7515d9e07bcad59d13bc3806628c6f841acb405c
panda-with-issues <ryoda94@gmail.com>
2019-06-14 11:58:59 +0200

    add a very_complicated_graph to test a_star, refactor dfs and bfs to work with weighted graph and return the number of visited vertices

--- graph_search_algorithms.py
+++ graph_search_algorithms.py
@@ -20,15 +20,18 @@ def dfs(graph, current_vertex, target, visited=None, path=None):
 
     path.append(current_vertex)
 
-    #base case
+    # base case
     if current_vertex == target:
-        return path
+        return path, len(visited)
 
-    #inductive step
+    # inductive step
     visited.append(current_vertex)
     
-    #recursive step
+    # recursive step
     for adjacent in graph[current_vertex]:
+        # using ignore as blind variable let the code run even with weighted graphs
+        if type(adjacent) == tuple:
+            adjacent, ignore = adjacent
         if adjacent not in visited:
             possible_path = dfs(graph, adjacent, target, visited, path)
             if possible_path:
@@ -50,13 +53,18 @@ def bfs(graph, origin, target):
     vertex_and_path = [origin, path]
     q = [vertex_and_path]
     visited = []
+    checked_count = 0
     while q:
         current_vertex, path = q.pop(0)
         visited.append(current_vertex)
         for adjacent in graph[current_vertex]:
+            # this use of ignore is the same of dfs'
+            if type(adjacent) == tuple:
+                adjacent, ignore = adjacent
             if adjacent not in visited:
+                checked_count += 1
                 if adjacent == target:
-                    return path + [adjacent]
+                    return path + [adjacent], checked_count
                 if adjacent not in [vertex_and_path[0] for vertex_and_path in q]:
                     q.append([adjacent, path + [adjacent]])
                 
@@ -99,7 +107,7 @@ def dijkstra(graph, origin, target):
                     paths[adjacent] = paths[current_vertex] + [adjacent]
     
     # path restitution
-    return paths.get(target)
+    return paths.get(target), len(visited)
     
 
 
@@ -137,13 +145,86 @@ dijkstra_graph = {
     'N': [('H', 7)]
 }
 
-print(dijkstra(dijkstra_graph, 'A', 'E'))
-print(dijkstra(dijkstra_graph, 'G', 'N'))
-print(dijkstra(dijkstra_graph, 'B', 'D'))
-print(dijkstra(dijkstra_graph, 'A', 'C'))
-print(dijkstra(dijkstra_graph, 'F', 'C'))
-print(dijkstra(dijkstra_graph, 'F', 'A'))
-print(dijkstra(dijkstra_graph, 'A', 'H'))
-print(dijkstra(dijkstra_graph, 'B', 'C'))
-print(dijkstra(dijkstra_graph, 'B', 'F'))
-print(dijkstra(dijkstra_graph, 'B', 'E'))
+very_complicate_graph = {
+    'A': [('B', 1), ('C', 3), ('D', 2), ('E', 2), ('F', 6)],
+    'B': [('A', 1), ('C', 6), ('G', 8), ('H', 3)],
+    'C': [('A', 3), ('B', 6), ('I', 5), ('J', 6), ('K', 7), ('L', 7)],
+    'D': [('A', 2), ('L', 1)],
+    'E': [('A', 2), ('M', 2), ('N', 4)],
+    'F': [('A', 6), ('O', 10)],
+    'G': [('B', 8), ('P', 2)],
+    'H': [('B', 3), ('Q', 1), ('R', 4)],
+    'I': [('C', 5), ('S', 3), ('T', 10), ('U', 8)],
+    'J': [('C', 6), ('W', 1)],
+    'K': [('C', 7), ('X', 8), ('Y', 3)],
+    'L': [('C', 7), ('D', 1), ('Z', 6), ('AA', 8), ('AB', 7), ('AC', 9), ('AD', 1)],
+    'M': [('E', 2), ('AD', 1), ('AE', 2), ('AF', 1)],
+    'N': [('E', 4), ('AG', 1), ('AH', 6), ('AI', 8)],
+    'O': [('F', 10), ('AJ', 6)],
+    'P': [('G', 2)],
+    'Q': [('H', 1)],
+    'R': [('H', 4)],
+    'S': [('I', 3)],
+    'T': [('I', 10)],
+    'U': [('I', 8)],
+    'V': [('W', 3), ('X', 9)],
+    'W': [('J', 1), ('V', 3)],
+    'X': [('K', 8), ('V', 9)],
+    'Y': [('K', 3)],
+    'Z': [('L', 6)],
+    'AA': [('L', 8)],
+    'AB': [('L', 7)],
+    'AC': [('L', 9)],
+    'AD': [('L', 1), ('M', 1)],
+    'AE': [('M', 2)],
+    'AF': [('M', 1)],
+    'AG': [('N', 1)],
+    'AH': [('N', 6)],
+    'AI': [('N', 8)],
+    'AJ': [('O', 6)]
+}
+
+coordinates = {
+    # mapped as (x, y)
+    'A': (17, 14),
+    'B': (17, 18),
+    'C': (22, 16),
+    'D': (20, 11),
+    'E': (13, 11),
+    'F': (13, 17),
+    'G': (14, 22),
+    'H': (20, 22),
+    'I': (25, 20),
+    'J': (26, 17),
+    'K': (24, 14),
+    'L': (22, 8),
+    'M': (14, 7),
+    'N': (9, 8),
+    'O': (9, 18),
+    'P': (12, 27),
+    'Q': (18, 28),
+    'R': (24, 25),
+    'S': (26, 24),
+    'T': (28, 22),
+    'U': (29, 19),
+    'V': (34, 13),
+    'W': (30, 15),
+    'X': (29, 12),
+    'Y': (25, 9),
+    'Z': (26, 5),
+    'AA': (22, 4),
+    'AB': (19, 4),
+    'AC': (16, 3),
+    'AD': (0, 16),
+    'AE': (12, 2),
+    'AF': (10, 4),
+    'AG': (6, 4),
+    'AH': (4, 9),
+    'AI': (7, 12),
+    'AJ': (4, 18)
+}
+
+print(dfs(graph, 'A', 'E'))
+print(dfs(very_complicate_graph, 'A', 'AH'))
+print(bfs(graph, 'A', 'E'))
+print(bfs(very_complicate_graph, 'A', 'AH'))

ef3b00a43fc5e436452b227d5239070a9feccee0
panda-with-issues <ryoda94@gmail.com>
2019-08-29 19:02:07 +0200

    add .pad() and .has() methods

--- Web_development/JS_exercises/Objects_Exercises/lodash.js
+++ Web_development/JS_exercises/Objects_Exercises/lodash.js
@@ -38,5 +38,59 @@ const _ = {
       end = temp
     }
     return n >= start && n < end
+  },
+  /*
+  * Task 4:
+  * The next string method we will implement is .pad(). .pad() takes three arguments: a string, a length and a str of characters to pad with.
+  * .pad() adds spaces evenly to both sides of the string to make it reach the desired length
+  * Extra padding is added to the end of the string if an odd amount of padding is required to reach the specified length
+  */
+  pad (str, length, char) {
+    if (typeof char === 'undefined') {
+      char = ' '
+    }
+    if (str.length < length) {
+      const padLength = length - str.length
+      let pad = ''
+      for (let i = 0; i < Math.floor(padLength / 2); i++) {
+        const charToPush = char[i % char.length]
+        pad += charToPush
+      }
+      if (padLength % 2 === 1) {
+        const padL = pad + char[pad.length % char.length]
+        return pad + str + padL
+      }
+      return pad + str + pad
+    }
+    return str
+  },
+  has (obj, path) {
+    if (typeof path === 'string') {
+      path = path.split(/[.[\]']/) // match . [ ] '
+      // clean from empty substrings
+      for (let i = path.length - 1; i >= 0; i--) {
+        if (path[i] === '') {
+          path.splice(i, 1)
+        }
+      }
+    }
+    // base case
+    if (path.length === 1) {
+      return Object.keys(obj).includes(path[0])
+    }
+    // inductive step
+    const nextProperty = path.shift()
+    obj = obj[nextProperty]
+    return this.has(obj, path)
   }
 }
+
+const test = {
+  a: 'minchia',
+  b: {
+    'sti cazzi': 'wow',
+    c: 'pene'
+  }
+}
+
+console.log(_.has(test, "c"))

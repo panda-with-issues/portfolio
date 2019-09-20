45d9ab4c0921fe97968706f6767d0fe1c4f58f66
panda-with-issues <ryoda94@gmail.com>
2019-09-20 17:16:57 +0200

    trying to implement handlebars.js

--- Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
+++ Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
@@ -1,19 +1,25 @@
 import keys from './api_keys.js'
 
+// Intanciate new errors
+const RequestFailedE = new Error('Request failed') // this will be thrown if an API request is rejected
+
 // get page information
 const destination = document.getElementById('destination').value
 const submitButton = document.getElementById('submit-button')
+const context = {
+  locations: []
+}
 
 // Foursquare informations
 const FSEndpoint = 'https://api.foursquare.com/v2/venues/explore' // this API lets you find interesting places near a given place
 const version = '&v=20190911' // required param to resolve the request. It points to the API version utilised. Format is YYYYMMDD. See Foursquare documentation under Versioning
 const FSQueryKey = '&near='
 const limit = '&limit=5' // return up to five venues
+const FSIconSize = '64'
 
 // APIXU informations
-const APIXUEndpoint = 'http://api.weatherstack.com/forecast' // this API lets you see what'll the weather be like in up to 14 days in the future
+const APIXUEndpoint = 'http://api.weatherstack.com/current' // this API lets you see what's theweather like in that location
 const APIXUQueryKey = '&query='
-const APIXULang = '&language=it' // display results in italian
 
 // AJAX
 async function getVenues () {
@@ -22,15 +28,49 @@ async function getVenues () {
   try {
     const response = await fetch(url)
     if (response.ok) {
-      const JsonResponse = await response.json()
-      const venues = JsonResponse.response.groups[0].items.map(element => element.venue)
-      return venues
+      const jsonResponse = await response.json()
+      const venues = jsonResponse.response.groups[0].items.map(element => element.venue)
+      // populating context with fetched data
+      venues.forEach(venue => {
+        const venueObj = {}
+        venueObj.name = venue.name
+        venueObj.address = venue.location.address
+        const icon = venue.categories[0].icon
+        venueObj.icon = icon.prefix + FSIconSize + icon.suffix
+        context.locations.push(venueObj)
+      })
+    } else {
+      throw RequestFailedE
     }
-    throw new Error('Request failed')
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+async function getWeather () {
+  // make a GET request to obtain current weather at the given destination
+  const url = APIXUEndpoint + keys.APIXUKey + APIXUQueryKey + destination
+  try {
+    const response = await fetch(url)
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      const weather = jsonResponse.current
+      return weather
+    }
+    throw RequestFailedE
   } catch (e) {
     console.log(e)
   }
 }
 
 // execute functions
-submitButton.onclick = getVenues
+function search () {
+  getVenues().then(renderVenues)
+  // handlebars boilerplate
+  const source = document.getElementById('handlebars').innerHTML
+  const template = Handlebars.compile(source)
+  const compiledHtml = template(context)
+  document.getElementById('hb-container').innerHTML = compiledHtml
+}
+
+submitButton.onclick = search

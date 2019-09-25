012fe2c14fcef5c7d2d180cb9a31f7cf06ca2b62
panda-with-issues <ryoda94@gmail.com>
2019-09-25 18:28:53 +0200

    finish logic to show locations and begin styling location cards

--- Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
+++ Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
@@ -4,8 +4,10 @@ import keys from './api_keys.js'
 const RequestFailedE = new Error('Request failed') // this will be thrown if an API request is rejected
 
 // get page information
-const destination = document.getElementById('destination').value
 const submitButton = document.getElementById('submit-button')
+const input = document.getElementById('destination-field')
+
+// Handlebars.js context
 const context = {
   locations: []
 }
@@ -15,6 +17,7 @@ const FSEndpoint = 'https://api.foursquare.com/v2/venues/explore' // this API le
 const version = '&v=20190911' // required param to resolve the request. It points to the API version utilised. Format is YYYYMMDD. See Foursquare documentation under Versioning
 const FSQueryKey = '&near='
 const limit = '&limit=5' // return up to five venues
+const FSGrayBG = 'bg_' // sets the icon backgrond color to gray. Must be set before FSIconSize
 const FSIconSize = '64'
 
 // APIXU informations
@@ -24,6 +27,7 @@ const APIXUQueryKey = '&query='
 // AJAX
 async function getVenues () {
   // make a GET request to Foursquare API to obtain interesting place near the destination variable
+  const destination = input.value
   const url = FSEndpoint + keys.FSClientId + keys.FSSecret + version + FSQueryKey + destination + limit
   try {
     const response = await fetch(url)
@@ -31,12 +35,13 @@ async function getVenues () {
       const jsonResponse = await response.json()
       const venues = jsonResponse.response.groups[0].items.map(element => element.venue)
       // populating context with fetched data
+      context.destination = destination
       venues.forEach(venue => {
         const venueObj = {}
         venueObj.name = venue.name
         venueObj.address = venue.location.address
         const icon = venue.categories[0].icon
-        venueObj.icon = icon.prefix + FSIconSize + icon.suffix
+        venueObj.icon = icon.prefix + FSGrayBG + FSIconSize + icon.suffix
         context.locations.push(venueObj)
       })
     } else {
@@ -63,14 +68,25 @@ async function getWeather () {
   }
 }
 
-// execute functions
-function search () {
-  getVenues().then(renderVenues)
-  // handlebars boilerplate
+function renderVenues () {
+  // boiler plate code for handlebars.js that render location data
   const source = document.getElementById('handlebars').innerHTML
   const template = Handlebars.compile(source)
   const compiledHtml = template(context)
   document.getElementById('hb-container').innerHTML = compiledHtml
 }
 
-submitButton.onclick = search
+// execute functions
+function search () {
+  // reset context in case of second search
+  if (context.locations.length) {
+    context.locations = []
+  }
+  getVenues().then(renderVenues)
+}
+
+submitButton.onclick = (e) => {
+  // this lets the search starts either with a click either with pressing enter
+  e.preventDefault()
+  search()
+}

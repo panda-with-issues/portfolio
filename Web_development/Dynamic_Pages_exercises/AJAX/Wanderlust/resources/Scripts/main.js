bcde6c18089e692973f018cce6c1cee6ebc83ff6
panda-with-issues <ryoda94@gmail.com>
2019-09-27 10:52:09 +0200

    add logic to sanitize user input; refactor code grouping helper functions

--- Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
+++ Web_development/Dynamic_Pages_exercises/AJAX/Wanderlust/resources/Scripts/main.js
@@ -6,7 +6,7 @@ const RequestFailedE = new Error('Request failed') // this will be thrown if an 
 
 // get page information
 const submitButton = document.getElementById('submit-button')
-const input = document.getElementById('destination-field')
+const inputField = document.getElementById('destination-field')
 
 // Handlebars.js context
 const context = {
@@ -14,50 +14,24 @@ const context = {
   days: []
 }
 
-// AJAX
-async function getVenues () {
-  // make a GET request to Foursquare API to obtain interesting place near the destination variable
-  // Foursquare informations
-  const endpoint = 'https://api.foursquare.com/v2/venues/explore' // this API lets you find interesting places near a given place
-  const version = '&v=20190911' // required param to resolve the request. It points to the API version utilised. Format is YYYYMMDD. See Foursquare documentation under Versioning
-  const queryKey = '&near='
-  const limit = '&limit=5' // return up to five venues
-  const grayBG = 'bg_' // sets the icon backgrond color to gray. Must be set before FSIconSize
-  const iconSize = '64'
-  const destination = input.value
-  const url = endpoint + keys.FSClientId + keys.FSSecret + version + queryKey + destination + limit
-  // fetch data
-  try {
-    const response = await fetch(url)
-    if (response.ok) {
-      const jsonResponse = await response.json()
-      const venues = jsonResponse.response.groups[0].items.map(element => element.venue)
-      // populate context with fetched data
-      context.destination = destination
-      venues.forEach(venue => {
-        const venueObj = {}
-        venueObj.name = venue.name
-        venueObj.address = venue.location.address
-        const icon = venue.categories[0].icon
-        venueObj.icon = icon.prefix + grayBG + iconSize + icon.suffix
-        context.locations.push(venueObj)
-      })
-    } else {
-      throw RequestFailedE
-    }
-  } catch (e) {
-    console.log(e)
-  }
+// helper functions
+
+function getDestination () {
+  // sanitize user input
+  const input = inputField.value.trim()
+  const sanitizedInput = input[0].toUpperCase() + input.slice(1)
+  return sanitizedInput
 }
 
 function formatTime (dateStr) {
-  // takes the string date given by OpenWeather JSON response and format it in user friendly layout
+  // takes the time infos by the string date given by OpenWeather JSON response and format it in user friendly layout
   const time = parseInt(dateStr.split(' ')[1].slice(0, 2), 10)
   const timeFormatted = `${time}:00 - ${time + 3}:00`
   return timeFormatted
 }
 
 function formatDate (dateStr) {
+  // takes the date by the string date given by OpenWeather JSON response and format it in user friendly layout
   const date = dateStr.split(' ')[0].split('-').reverse()
   switch (date[1]) {
     case '01':
@@ -101,13 +75,57 @@ function formatDate (dateStr) {
   return dateFormatted
 }
 
+function renderData () {
+  // boiler plate code for handlebars.js
+  const source = document.getElementById('handlebars').innerHTML
+  const template = Handlebars.compile(source)
+  const compiledHtml = template(context)
+  document.getElementById('hb-container').innerHTML = compiledHtml
+}
+
+// AJAX
+async function getVenues () {
+  // make a GET request to Foursquare API to obtain interesting place near the destination variable
+  // Foursquare informations
+  const endpoint = 'https://api.foursquare.com/v2/venues/explore' // this API lets you find interesting places near a given place
+  const version = '&v=20190911' // required param to resolve the request. It points to the API version utilised. Format is YYYYMMDD. See Foursquare documentation under Versioning
+  const queryKey = '&near='
+  const limit = '&limit=5' // return up to five venues
+  const grayBG = 'bg_' // sets the icon backgrond color to gray. Must be set before FSIconSize
+  const iconSize = '64'
+  const destination = getDestination()
+  const url = endpoint + keys.FSClientId + keys.FSSecret + version + queryKey + destination + limit
+  // fetch data
+  try {
+    const response = await fetch(url)
+    if (response.ok) {
+      const jsonResponse = await response.json()
+      const venues = jsonResponse.response.groups[0].items.map(element => element.venue)
+      // populate context with fetched data
+      context.destination = destination
+      venues.forEach(venue => {
+        const venueObj = {}
+        venueObj.name = venue.name
+        venueObj.address = venue.location.address
+        const icon = venue.categories[0].icon
+        venueObj.icon = icon.prefix + grayBG + iconSize + icon.suffix
+        context.locations.push(venueObj)
+      })
+    } else {
+      throw RequestFailedE
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 async function getWeather () {
   // make a GET request to obtain current weather at the given destination
   // OpenWeather informations
   const endpoint = 'https://api.openweathermap.org/data/2.5/forecast' // this API lets you see what'll the weather be like in that location in next five days
   const queryKey = '&q='
   const celsius = '&units=metric' // set results in celsius degrees
-  const destination = input.value
+  const destination = getDestination()
   // retrieve country code
   let queryString
   for (let i = 0; i < cities.length; i++) {
@@ -145,14 +163,6 @@ async function getWeather () {
   }
 }
 
-function renderData () {
-  // boiler plate code for handlebars.js that render location data
-  const source = document.getElementById('handlebars').innerHTML
-  const template = Handlebars.compile(source)
-  const compiledHtml = template(context)
-  document.getElementById('hb-container').innerHTML = compiledHtml
-}
-
 // execute functions
 function search () {
   // reset context in case of second search
@@ -161,6 +171,7 @@ function search () {
     context.days = []
   }
   Promise.all([getVenues(), getWeather()]).then(renderData)
+  inputField.value = ''
 }
 
 submitButton.onclick = (e) => {

140f763ff3af1cf17714af2842ca48bfb52e44d1
panda-with-issues <ryoda94@gmail.com>
2020-03-14 10:53:41 +0100

    complete SearchBar component

--- Web_development/Dynamic_Pages_exercises/React_Exercises/ravenous/src/components/SearchBar/SearchBar.js
+++ Web_development/Dynamic_Pages_exercises/React_Exercises/ravenous/src/components/SearchBar/SearchBar.js
@@ -9,6 +9,30 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
   renderSortByOptions () {
-    
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionsValue = sortByOptions[sortByOption]
+      return <li key={sortByOptionsValue}>{sortByOption}</li>
+    })
+  }
+
+  render () {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()} // sono molto insicura di questc
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    )
   }
 }
+
+export default SearchBar

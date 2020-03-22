d3ad7a575b7ef06ff24dd51811f876f2eb5ffc57
panda-with-issues <ryoda94@gmail.com>
2020-03-22 11:47:40 +0100

    complete part 3

--- Web_development/Dynamic_Pages_exercises/React_Exercises/ravenous/src/components/SearchBar/SearchBar.js
+++ Web_development/Dynamic_Pages_exercises/React_Exercises/ravenous/src/components/SearchBar/SearchBar.js
@@ -1,34 +1,74 @@
 import React from 'react'
 import './SearchBar.css'
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-}
-
 class SearchBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    }
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    }
+    this.handleLocationChange = this.handleLocationChange.bind(this)
+    this.handleTermChange = this.handleTermChange.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  getSortByClass (sortByOption) {
+    return this.state.sortBy === sortByOption ? 'active' : ''
+  }
+
+  handleSortByChange (sortByOption) {
+    this.setState({ sortBy: sortByOption })
+  }
+
+  handleTermChange (e) {
+    this.setState({ term: e.target.value })
+  }
+
+  handleLocationChange (e) {
+    this.setState({ location: e.target.vaue })
+  }
+
+  handleSearch (e) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+    e.preventDefault()
+  }
+
   renderSortByOptions () {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionsValue = sortByOptions[sortByOption]
-      return <li key={sortByOptionsValue}>{sortByOption}</li>
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      const sortByOptionValue = this.sortByOptions[sortByOption]
+      return (
+        <li
+          key={sortByOptionValue}
+          className={this.getSortByClass(sortByOptionValue)}
+          onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+        >
+          {sortByOption}
+        </li>
+      )
     })
   }
 
   render () {
     return (
-      <div className="SearchBar">
-        <div className="SearchBar-sort-options">
+      <div className='SearchBar'>
+        <div className='SearchBar-sort-options'>
           <ul>
             {this.renderSortByOptions()}
           </ul>
         </div>
-        <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+        <div className='SearchBar-fields'>
+          <input onChange={this.handleTermChange} placeholder='Search Businesses' />
+          <input onChange={this.handleLocationChange} placeholder='Where?' />
         </div>
-        <div className="SearchBar-submit">
-          <a>Let's Go</a>
+        <div className='SearchBar-submit'>
+          <a onClick={this.handleSearch}>Let's Go</a>
         </div>
       </div>
     )
